375e2f835bcd2392f5c9450e33a9f8bf1ac4d441
zygame1314 <zygame1314@gmail.com>
2025-11-30 23:24:22 +0800

    更新sendEmail函数：将SMTP主机更改为smtp.qiye.aliyun.com

--- functions/smtp.js
+++ functions/smtp.js
@@ -5,7 +5,7 @@ export async function sendEmail(env, to, subject, html) {
     throw new Error("请配置 SMTP_USER 和 SMTP_PASS 环境变量");
   }
   const socket = connect({
-    hostname: 'smtp.zygame1314.site',
+    hostname: 'smtp.qiye.aliyun.com',
     port: 465,
     secureTransport: 'on',
   });
