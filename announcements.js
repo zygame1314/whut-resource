af0993d77068b59cc5c225e0b211fd070aa726ba
zygame1314 <zygame1314@gmail.com>
2025-11-30 17:01:07 +0800

    优化公告表单样式：添加公告表单容器样式，改进表单元素布局和交互效果，增强用户体验。

--- announcements.js
+++ announcements.js
@@ -214,6 +214,7 @@ function formatDateLocal(dateString) {
         month: '2-digit',
         day: '2-digit',
         hour: '2-digit',
-        minute: '2-digit'
+        minute: '2-digit',
+        timeZone: 'Asia/Shanghai'
     });
 }
