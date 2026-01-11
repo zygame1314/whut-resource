5c41dfd6d8d1b4e80a2bbeb535c9d9191da9e3ac
zygame1314 <zygame1314@gmail.com>
2026-01-11 19:11:35 +0800

    feat: 实现基于数据库的动态维护模式并支持管理员绕过，修复 CORS 预检失败问题

--- functions/utils.js
+++ functions/utils.js
@@ -60,7 +60,7 @@ export function addCorsHeaders(headers = {}) {
     ...headers,
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With, Cache-Control, Pragma',
     'Access-Control-Max-Age': '86400',
   };
 }
