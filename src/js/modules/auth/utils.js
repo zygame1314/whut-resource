6ed94bf8cdeb6157cbaf695a212ccd88212a9d2e
CNproudly <l160600248@outlook.com>
2026-02-22 15:11:20 +0800

    feat: add liked state styling for like button and improve announcement footer layout

--- src/js/modules/auth/utils.js
+++ src/js/modules/auth/utils.js
@@ -19,7 +19,9 @@ function formatDateLocal(dateString) {
     } else {
         date = new Date(dateString);
     }
+    if (Number.isNaN(date.getTime())) return '';
     return date.toLocaleString('zh-CN', {
+        year: 'numeric',
         month: '2-digit',
         day: '2-digit',
         hour: '2-digit',
